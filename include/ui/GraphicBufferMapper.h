CyanogenMod/android_frameworks_native

exynos4: getphys implementation on buffermanager for samsung omx
Change-Id: Iee665f56aaf601479d796703db983ca5fce9c5c3

Conflicts:
	include/media/hardware/HardwareAPI.h
	include/ui/GraphicBufferMapper.h
	libs/ui/GraphicBufferMapper.cpp

include/ui/GraphicBufferMapper.h
@@ -50,6 +50,10 @@ public:
 
     status_t unlock(buffer_handle_t handle);
     
+#ifdef EXYNOS4210_ENHANCEMENTS
+    status_t getphys(buffer_handle_t handle, void** paddr);
+#endif
+
     // dumps information about the mapping of this handle
     void dump(buffer_handle_t handle);
 

rINanDO/android_frameworks_native-du

Change-Id: I73d9633e1a7b3316ede28dd1369f8f069609cbb0

include/ui/GraphicBufferMapper.h
@@ -59,6 +59,10 @@ public:
 
     status_t unlockAsync(buffer_handle_t handle, int *fenceFd);
 
+#ifdef EXYNOS4_ENHANCEMENTS
+    status_t getphys(buffer_handle_t handle, void** paddr);
+#endif
+
     // dumps information about the mapping of this handle
     void dump(buffer_handle_t handle);
 